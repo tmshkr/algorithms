f6932690cb2ed4427c7f73b6a9d62bd2a4f52fc4
Tim Shaker <tim@tmshkr.com>
2020-03-13 21:37:30 -0700

    update mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -33,5 +33,5 @@ function mergeSort(arr1, arr2) {
   return results;
 }
 
-const result = mergeSort([1, 10, 50], [2, 14, 99, 100]); // [1, 2, 10, 14, 50, 99, 100]
-console.log(result);
+const result = mergeSort([1, 10, 50], [2, 14, 99, 100]);
+console.log(result); // [1, 2, 10, 14, 50, 99, 100]
